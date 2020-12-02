a4f7c10193315b6dd6664a00a7d733ffc5e3f556
Youpen-y <1586590320@qq.com>
2020-12-02 10:09:05 +0800

    add

---
+++ voteapp/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+namespace voteapp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string useraccount;
+        public string userpassword;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //Connect to the mysql database
+           
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void registerButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+            this.Close();
+        }
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            //获取登录信息
+            useraccount = this.uText.Text;      //用户名
+            userpassword = this.pText.Password;         //用户密码
+            //验证数据（非空）
+            if (useraccount.Equals("") || userpassword.Equals(""))
+            {
+                MessageBox.Show("账号或密码不能为空！");
+            }
+            else
+            {
+                String connetStr = "server=127.0.0.1;port=3306;user=root;password=yyp5689;database=vote;";
+                MySqlConnection conn = new MySqlConnection(connetStr);
+                try
+                {
+                    //打开数据库
+                    conn.Open();    
+                    //Mysql 查询语句
+                    string strSql = "select count(*) from user_table where userAccount = '"+useraccount+"' and userPassword = '"+userpassword+"'";
+                    MySqlCommand comm = new MySqlCommand(strSql, conn);
+                    //判断是否登录成功
+                    if(Convert.ToInt32(comm.ExecuteScalar()) > 0)
+                    {
+                        MessageBox.Show("登陆成功！");
+                        Window2 window2 = new Window2();
+                        window2.Show();
+                        //跳转至主页面
+                        this.Hide();
+                    }
+                    //账号和密码验证错误，提示错误
+                    else
+                    {
+                        MessageBox.Show("账号或密码错误！");
+                    }
+                    
+                }
+                //提示数据库错误
+                catch (MySqlException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    //关闭数据库
+                    conn.Close();
+                }
+            }
+        }
+
+
+        private void pText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+
+}
---
+++ voteapp/Window1.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace voteapp
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window1 : Window
+    {
+
+        string registeraccount;
+        string registerpassword;
+        string registercheck;
+        string registername ;
+        string registertext;
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            registeraccount = this.account.Text;
+            registerpassword = this.password.Password;
+            registercheck = this.check.Password;
+            registername = this.nickname.Text;
+            registertext = this.yourtext.Text;
+
+
+            if (registeraccount.Equals("") || registerpassword.Equals(""))
+            {
+                MessageBox.Show("账号或密码不能为空");
+            }
+            else if (!registercheck.Equals(registerpassword))
+            {
+                MessageBox.Show("密码不一致，请重新输入！");
+            }
+            else
+            {
+                String connetStr = "server=127.0.0.1;port=3306;user=root;password=yyp5689;database=vote;";
+                MySqlConnection conn = new MySqlConnection(connetStr);
+                try
+                {
+                    //打开数据库
+                    conn.Open();
+                    //Mysql 查询语句
+                    string strSql = "select count(*) from user_table where userAccount = '"+registeraccount+"'";
+                    MySqlCommand comm = new MySqlCommand(strSql, conn);
+                    //判断是否已存在帐号
+                    if (Convert.ToInt32(comm.ExecuteScalar()) > 0)
+                    {
+                        MessageBox.Show("当前账号已存在，请重新输入！");
+                    }
+                    else
+                    {
+                        try {
+                            string strInsertSql = "insert into user_table (userAccount, userPassword, userNickname, userText) values ('"+registeraccount+"','"+ registerpassword +"','"+ registername +"','"+ registertext+"')";
+                            //string str = "insert into user_table (userAccount, userPassword, userNickname, userText) values ('wj','123','wangjin','noway')";
+                            //string strInsertSql = "insert into user_table （userAccount, userPassword, userNickname, userText) value(@registeraccount, @registerpassword, @registername, @registertext)";
+                            // string strInsertSql = "insert into user_table （userAccount, userPassword, userNickname, userText) value('wj', '123456' , 'wangjin' , 'don't do that')";
+                            MySqlCommand command = new MySqlCommand(strInsertSql, conn);
+                            //MySqlCommand command = new MySqlCommand(str, conn);
+                            if (command.ExecuteNonQuery() > 0)
+                            {
+                                MessageBox.Show("账号注册成功");
+                            }
+                            else
+                            {
+                                MessageBox.Show("账号注册失败1");
+                            }
+
+                        } catch
+                        {
+                            MessageBox.Show("账号注册失败2");
+                        } 
+                        
+                    }
+                }
+                catch (MySqlException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        { 
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Hide();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
